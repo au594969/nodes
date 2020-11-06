3b79640ea4f600b33adb0b5295fa395a4552a9e2
au594969 <cml90@live.dk>
2020-11-06 12:45:09 +0100

    reset node imp. +  model size edit

--- au_dynamixel_reset.py
+++ au_dynamixel_reset.py
@@ -11,46 +11,11 @@ from trajectory_msgs.msg import JointTrajectory
 import math
 
 def invkin(xyz):
-	"""
-	Python implementation of the the inverse kinematics for the crustcrawler
-	Input: xyz position
-	Output: Angels for each joint: q1,q2,q3,q4
 	
-	You might adjust parameters (d1,a1,a2,d4).
-	The robot model shown in rviz can be adjusted accordingly by editing 		au_crustcrawler_ax12.urdf
-	"""
-
-	d1 = 12.5 # cm (height of 2nd joint)
-	a1 = 0.0 # (distance along "y-axis" to 2nd joint)
-	a2 = 16.8 # (distance between 2nd and 3rd joints)
-	d4 = 23.5 # (distance from 3rd joint to gripper center - all inclusive, ie. also 4th joint)
-
-	# Calculate oc
-	oc = xyz # - d4*R*[0;0;1]
-	xc = oc[0]
-	yc = oc[1]
-	zc = oc[2]
-	
-	
-	# Calculate q1
-	q1 = math.atan2(yc, xc)  # NOTE: Order of y and x.. depend on math.atan2 func..
-	
-	
-	# Calculate q2 and q3
-	r2 = (xc - a1*math.cos(q1))**2 + (yc - a1*math.sin(q1))**2 	# radius squared - radius can never be negative, q1 accounts for this..
-	s = (zc - d1) 												# can be negative ! (below first joint height..)
-	D = ( r2 + s**2 - a2**2 - d4**2)/(2*a2*d4)   				# Eqn. (3.44)
-	
-	q3 = math.atan2(-math.sqrt(1-D**2), D) 						#  Eqn. (3.46)
-	q2 = math.atan2(s, math.sqrt(r2)) - math.atan2(d4*math.sin(q3), a2 + d4*math.cos(q3)) # Eqn. (3.47)
-	
-	
-	# calculate q4 - ie. rotation part
-	# r32 = R(3,2);
-	# r23 = R(2,3);
-	# c23 = math.cos(q2 + q3);
-	# q4 = math.atan2(r23/c23, r32/c23); 
-	q4 = 0 # not consider rotation so far..
+	q1 = 0
+	q2 = 0
+	q3 = 0
+	q4 = 0
 
 	print(q1,-q2,q3,q4)
 	return q1,-q2,q3,q4
@@ -74,8 +39,7 @@ class ActionExampleNode:
 
 		yarr = [-20]*len(xarr)
 
-		zarr = [1]*len(xarr)
-		
+		zarr = [5]*len(xarr)
 
 
 
@@ -106,7 +70,7 @@ class ActionExampleNode:
 		print(self.client.get_result())
 
 if __name__ == "__main__":
-	rospy.init_node("au_dynamixel_invkin_trial_node")
+	rospy.init_node("au_dynamixel_invkin_reset")
 
 	node= ActionExampleNode("/arm_controller/follow_joint_trajectory")
 
